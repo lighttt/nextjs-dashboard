5c7a68a8fe4a57d0cf8f67f8eca0a81e05f3ef62
lighttt <manish.eclipse@gmail.com>
2024-01-16 14:38:19 +1100

    Add validation and error handling for invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,44 +6,111 @@ import { z } from 'zod';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce.number().gt(0, {
+    message: 'Please enter an amount greater than 0.',
+  }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
 const CreatInvoice = FormSchema.omit({ id: true, date: true });
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreatInvoice.parse(
-    Object.fromEntries(formData.entries()),
-  );
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Validate form data
+  const validateFields = CreatInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  // If validation fails, return errors early. Otherwise, continue.
+  if (!validateFields.success) {
+    return {
+      errors: validateFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validateFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`INSERT INTO invoices (customer_id, amount, status, date) 
+  try {
+    await sql`INSERT INTO invoices (customer_id, amount, status, date) 
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+  } catch (err) {
+    console.log(err);
+    return {
+      message: 'Database Error: Failed to create invoice.',
+    };
+  }
 
+  // Revalidate the cache for the invoices page
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse(
-    Object.fromEntries(formData.entries()),
-  );
-  const amountInCents = amount * 100;
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
+  // Validate form data
+  const validateFields = UpdateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
 
-  await sql`UPDATE invoices SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status} WHERE id = ${id}`;
+  // If validation fails, return errors early. Otherwise, continue.
+  if (!validateFields.success) {
+    return {
+      errors: validateFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
 
+  try {
+    const { customerId, amount, status } = validateFields.data;
+    const amountInCents = amount * 100;
+
+    await sql`UPDATE invoices SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status} WHERE id = ${id}`;
+  } catch (err) {
+    console.log(err);
+    return {
+      message: 'Database Error: Failed to update invoice.',
+    };
+  }
+  // Revalidate the cache for the invoices page
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (err) {
+    console.log(err);
+    return {
+      message: 'Database Error: Failed to delete invoice.',
+    };
+  }
+  // Revalidate the cache for the invoices page
   revalidatePath('/dashboard/invoices');
 }
